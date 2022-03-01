22f75dc470b7c1034a84b1f2dbcf430b76b9c72e
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2022-02-28 22:44:28 -0700

    Added by Ryan

---
+++ ismission7RyanPinkney/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ismission7RyanPinkney.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ismission7RyanPinkney.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            basket = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
